b5d88a384a1391ccb4a08c2a5647971b79c3f228
Joelle  Johnson <joellej221@gmail.com>
2023-11-27 10:08:38 -0500

    Added another origin

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.json())
 app.use(cors({
-    origin: ['http://localhost:3001', ]
+    origin: ['http://localhost:3001', 'http://localhost:3000']
 }))
 
 //phase 1, add and get items from wishlist
