06b88feb34f46a3ae080004fc663363eda2fb3de
Joelle  Johnson <joellej221@gmail.com>
2023-12-06 15:44:37 -0500

    CORS error fix

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ import { isAuthenticated } from "./ApiPoints/middleware.js";
 const PORT = process.env.PORT || 3001
 const app = express();
 
-app.use(express.json())
 app.use(cors({
-    origin: ['http://localhost:3001', 'http://localhost:3000', 'https://final-project-630f3.web.app']
+    origin: ['http://localhost:3001', 'http://localhost:3000', 'http://localhost:3002', 'https://final-project-630f3.web.app', 'https://final-project-630f3.web.app']
 }))
+app.use(express.json())
 
 //handle preflight OPTIONS reqs for root route
-app.options('/', cors()); 
-
+app.options('/', cors())
+app.options('/dashboard', cors())
 
 //phase 1, add and get items from wishlist
 app.get('/dashboard', isAuthenticated, getItemsFromWishlist)
