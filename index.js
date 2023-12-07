ef6623bc10c085bd5d47601612ab8f55eff9b287
Joelle  Johnson <joellej221@gmail.com>
2023-12-06 19:24:02 -0500

    Commiting too pull from main

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
 import express from "express"
 import cors from "cors"
-import { getItemsFromWishlist, addItemToWishlist } from "./ApiPoints/phase1.js";
-import { signup, login } from "./ApiPoints/signup.js"
+import { getItemsFromWishlist, addItemToWishlist } from "./ApiPoints/getAndAddItems.js";
+import { signup, login } from "./ApiPoints/signupAndLogin.js"
 import { updateIsPurchased, deleteListItem } from "./ApiPoints/updateAndDeleteItems.js";
+import { isAuthenticated } from "./ApiPoints/middleware.js";
+import { getFriendsWishlist } from "./ApiPoints/sharedLists.js";
 
 const PORT = process.env.PORT || 3001
 const app = express();
 
-app.use(express.json())
 app.use(cors({
-    origin: ['http://localhost:3001', 'http://localhost:3000', 'https://final-project-630f3.web.app']
+    origin: ['http://localhost:3001', 'http://localhost:3000', 'http://localhost:3002', 'https://final-project-630f3.web.app', 'https://final-project-630f3.web.app']
 }))
+app.use(express.json())
 
-//handle preflight OPTIONS reqs for root route
-app.options('/', cors()); 
+//handle preflight OPTIONS reqs (handle CORS errors)
+app.options('/', cors())
+app.options('/dashboard', cors())
 
-
-//phase 1, add and get items from wishlist
-app.get('/dashboard', getItemsFromWishlist)
-app.post('/dashboard', addItemToWishlist)
+//add and get items from your own wishlist
+app.get('/dashboard', isAuthenticated, getItemsFromWishlist)
+app.post('/dashboard', isAuthenticated, addItemToWishlist)
 
 
 //update and delete wishlist items:
 app.patch('/dashboard', updateIsPurchased)
-app.delete('/dashboard', deleteListItem)
+app.delete('/dashboard', isAuthenticated, deleteListItem)
 
 
 //signup and login
 app.post('/', signup)
 app.post('/login', login)
+
+//accessing friends wishlists
+app.post('/share/:userid', getFriendsWishlist)
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`))
