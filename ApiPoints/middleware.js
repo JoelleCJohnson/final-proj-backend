b40219880f5e2032bfc9e3a04b40524a83c0a153
Joelle  Johnson <joellej221@gmail.com>
2023-12-06 19:38:42 -0500

    Added middleware

---
+++ ApiPoints/middleware.js
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken'
+
+export async function isAuthenticated(req, res, next) {
+    const key = process.env.secretKey
+    const { authorization } = req.headers
+    if(!authorization){
+        res.status(401).send({ message: 'No authorization token found' })
+        return
+    }
+    jwt.verify(authorization, key, (err, decoded) => {
+        if(err){
+            res.status(401).send(err)
+            return
+        }
+        req.locals = decoded
+        next()
+    })
+}
